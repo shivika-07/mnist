1bdf6febc6922c3ea958f85610350f59e696d71e
Shivika Agarwal <agarwal.14@iitj.ac.in>
2021-09-24 06:53:25 +0530

    updated the file with model selection

--- mnist1/plot_digits_classification.py
+++ mnist1/plot_digits_classification.py
@@ -20,38 +20,11 @@ from sklearn import datasets, metrics, svm
 from sklearn.model_selection import train_test_split
 from skimage.transform import rescale
 import numpy as np
-
-
-###############################################################################
-# Digits dataset
-# --------------
-#
-# The digits dataset consists of 8x8
-# pixel images of digits. The ``images`` attribute of the dataset stores
-# 8x8 arrays of grayscale values for each image. We will use these arrays to
-# visualize the first 4 images. The ``target`` attribute of the dataset stores
-# the digit each image represents and this is included in the title of the 4
-# plots below.
-#
-# Note: if we were working from image files (e.g., 'png' files), we would load
-# them using :func:`matplotlib.pyplot.imread`.
+import os
+from joblib import dump, load
+from utils import *
 
 digits = datasets.load_digits()
-
-###############################################################################
-# Classification
-# --------------
-#
-# To apply a classifier on this data, we need to flatten the images, turning
-# each 2-D array of grayscale values from shape ``(8, 8)`` into shape
-# ``(64,)``. Subsequently, the entire dataset will be of shape
-# ``(n_samples, n_features)``, where ``n_samples`` is the number of images and
-# ``n_features`` is the total number of pixels in each image.
-#
-# We can then split the data into train and test subsets and fit a support
-# vector classifier on the train samples. The fitted classifier can
-# subsequently be used to predict the value of the digit for the samples
-# in the test subset.
 
 # flatten the images
 n_samples = len(digits.images)
@@ -62,64 +35,55 @@ data = digits.images.reshape((n_samples, -1))
 # Create a classifier: a support vector classifier
 
 for rescale_factor in rescale_factors:
-    gammaV = [0.0001,0.001,0.01,0.1,1]
+    gammaV = [10 ** exponent for exponent in range(-7, 0)]
 
-    # trainaccuracylst = []
-    # valaccuracylst = []
-    # testaccuracylst = []
     model_candidates = []
     for gamma in gammaV:
-        resized_images = []
-        for d in digits.images:
-            resized_images.append(rescale(d, rescale_factor, anti_aliasing=False))
-
+        resized_images = preprocess(digits.images,rescale_factor)
         resized_images = np.array(resized_images)
         data = resized_images.reshape((n_samples, -1))
 
         clf = svm.SVC(gamma=gamma)
 
         # Split data into 50% train and 50% test subsets
-        X_train, X_val, y_train, y_val = train_test_split(
-            data, digits.target, test_size=0.3, shuffle=False)
-
-        X_val, X_test, y_val, y_test = train_test_split(
-            X_val, y_val, test_size=0.5, shuffle=False)
-        
+        test_size,val_size = 0.15, 0.15
+        X_train, X_test, X_val,y_train,y_test,y_val = create_split(data, digits.target,test_size, val_size)       
 
         # Learn the digits on the train subset
         clf.fit(X_train, y_train)
 
-        predicted_valid = clf.predict(X_val)
-        acc_valid = metrics.accuracy_score(y_pred=predicted_valid, y_true=y_val)
-        f1_valid = metrics.f1_score(
-            y_pred=predicted_valid, y_true=y_val, average="macro"
-        )
+        metric_valid = test(clf, X_val, y_val)
 
-        if acc_valid < 0.11:
+        if metric_valid['acc'] < 0.11:
                 print("Skipping for {}".format(gamma))
                 continue
         
         candidate = {
-                "acc_valid": acc_valid,
-                "f1_valid": f1_valid,
+                "acc_valid": metric_valid['acc'],
+                "f1_valid": metric_valid['f1'],
                 "gamma": gamma,
             }
         model_candidates.append(candidate)
-        max_valid_f1_model_candidate = max(
-            model_candidates, key=lambda x: x["f1_valid"]
-        )
 
-        predicted = clf.predict(X_test)
+        # cwd = os.getcwd()
+        # print(cwd)
+        output_folder = "./mnist1/models/tt_{}_val_{}_rescale_{}_gamma_{}".format(
+                test_size, val_size, rescale_factor, gamma)
+        os.mkdir(output_folder)
+        dump(clf, os.path.join(output_folder,"model.joblib"))
 
-        acc = metrics.accuracy_score(y_pred=predicted, y_true=y_test)
-        f1 = metrics.f1_score(y_pred=predicted, y_true=y_test, average="macro")
-        print(
-            "{}x{}\t{}\t{:.3f}\t{:.3f}".format(
-                resized_images[0].shape[0],
-                resized_images[0].shape[1],
-                max_valid_f1_model_candidate["gamma"],
-                acc,
-                f1,
+    max_valid_f1_model_candidate = max(model_candidates, key=lambda x: x["f1_valid"])
+    best_model_folder="./mnist1/models/tt_{}_val_{}_rescale_{}_gamma_{}".format(
+                test_size, val_size, rescale_factor, max_valid_f1_model_candidate['gamma']
             )
+    metric = test(clf, X_test, y_test)
+    print(
+        "{}x{}\t{}\t{:.3f}\t{:.3f}".format(
+            resized_images[0].shape[0],
+            resized_images[0].shape[1],
+            max_valid_f1_model_candidate["gamma"],
+            metric['acc'],
+            metric['f1'],
         )
+    )
 
